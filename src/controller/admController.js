a6f224ad37d66bd7fa94f035c8ae705bc740c44a
davi <daviforever17@gmail>
2024-10-18 16:33:34 -0300

    ass.

--- src/controller/admController.js
+++ src/controller/admController.js
@@ -7,6 +7,31 @@ import consultarAdmService from "../service/adm/consutarAdmService.js";
 import { gerarToken } from "../utils/jwt.js";
 import { autenticar } from "../utils/jwt.js";
 
+
+endpoints.post('/cadastrar', async (req, resp) => {
+
+    try {
+    
+        let adm = req.body;
+
+        let id = await inserirAdmService(adm);
+
+        resp.send({
+
+            idAdm: id
+
+        })
+
+    }
+    catch(err){
+        
+        resp.status(400).send({
+            erro: err.message
+        })
+        
+    }
+})
+
 endpoints.post('/login', async (req, resp) => {
 
     try {
